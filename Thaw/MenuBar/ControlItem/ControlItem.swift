d8216cc28f728a462e69132b81c9b186d4dab740
Toni Förster <toni.foerster@icloud.com>
2026-02-03 11:13:19 +0100

    feat: reveal always-hidden section on thaw icon double-click
    Signed-off-by: Toni Förster <toni.foerster@icloud.com>

--- Thaw/MenuBar/ControlItem/ControlItem.swift
+++ Thaw/MenuBar/ControlItem/ControlItem.swift
@@ -493,6 +493,16 @@ final class ControlItem {
             // Running this from a Task seems to improve the visual
             // responsiveness of the status item's button.
             Task {
+                if
+                    event.clickCount > 1,
+                    identifier == .visible,
+                    let alwaysHidden = menuBarManager.section(withName: .alwaysHidden),
+                    alwaysHidden.isEnabled
+                {
+                    alwaysHidden.show()
+                    return
+                }
+
                 if modifierFlags == .control {
                     showMenu()
                     return
